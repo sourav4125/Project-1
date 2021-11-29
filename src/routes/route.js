be9d5a174b396e1ba4b1b9c8f7758713b9d1a4a9
arpitdhncve <arpit5028@gmail.com>
2021-11-29 09:26:15 +0530

    README of Project/blog

--- src/routes/route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
-});
-
-module.exports = router;
